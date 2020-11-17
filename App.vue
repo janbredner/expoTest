b98473c0f859faed3b8431eef6d3ebf691c7a19d
janbredner <jan-bredner@gmx.de>
2020-11-17 10:22:24 +0100

    commit before testing tailwind

--- App.vue
+++ App.vue
@@ -1,9 +1,24 @@
 <template>
   <view class="container">
     <text class="text-color-primary">My Vue Native App</text>
-    <text>asasdasdasd</text>
+    <text>hi!</text>
+    <text-input
+        :style="{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}"
+        v-model="text"
+    />
+    <text>{{text}}</text>
   </view>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      text: ''
+    };
+  }
+}
+</script>
 
 <style>
 .container {
